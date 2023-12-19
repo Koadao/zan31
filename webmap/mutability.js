0db8e0422cb8e1c3336fa0159dbc5f99bed6ea33
Tom M <151040049+Koadao@users.noreply.github.com>
2023-12-19 16:10:44 +0100

    Add files via upload

--- webmap/mutability.js
+++ webmap/mutability.js
@@ -12,7 +12,7 @@ function getColorarray(d, distribution) {
                             '#9002d0'; 
 }
 
-//breaks
+//breaks function
 function calculateEqualIntervals(values, numClasses) 
 {
     // Step 1: Find the minimum and maximum values in the data
@@ -37,10 +37,8 @@ function calculateEqualIntervals(values, numClasses)
     return classBreaks;
 }
 
-//var area_filter = 700
-
-//warning : data crs must be EPSG:4326
-var data_response = $.ajax({
+//GeoJSON response
+var data_response = $.ajax({//warning : data crs must be EPSG:4326
     url:"data/parc_amc3.geojson",
     dataType: "json",
     success: console.log("Parcels successfully found!"),
@@ -49,11 +47,13 @@ var data_response = $.ajax({
     }
 })
 
-//specify that this code should run once the county data request is complete
+//specify that this code should run once the parcels data request is complete
 $.when(data_response).done(function() {
 
+    //breaks choroplethe
     let values = data_response.responseJSON.features.map(feature => feature.properties['i_multi_crit']);
     console.log("i_multi_crit", values);
+    
     //Apply equal fonction
     let EqualInt = calculateEqualIntervals(values, 7)
     console.log("EqualIntervals", EqualInt);
@@ -70,6 +70,7 @@ $.when(data_response).done(function() {
         };
     }
     
+    //interactions functions
     function highlightFeature(e) {
         var layer = e.target;
 
@@ -101,12 +102,14 @@ $.when(data_response).done(function() {
         });
     }
 
+    //map var
     const map = L.map('map', {
         center:[43.6013, 1.4447],//Toulouse
         zoom:14,
         //layers:[osm]
     });
 
+    //base maps
     var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         minZoom: 8,
         maxZoom: 25,
@@ -121,12 +124,22 @@ $.when(data_response).done(function() {
     
     osm.addTo(map)
 
+    //layers
+    var area_filter = 3000;
+    
     var parcels_f = L.geoJSON(data_response.responseJSON,{
         style: style,
         onEachFeature: onEachFeature
     }).addTo(map);
 
     map.fitBounds(parcels_f.getBounds());
+
+    console.log("Parcels loaded", parcels.responseJSON);
+
+    var parcels_filt_response = geotoolbox.filter(parcels.responseJSON, (d) => d.area > area_filter);
+    console.log("Area filter applied", parcels_filt_response);
+
+    var parcels_fil = L.geoJSON(parcels_filt_response).addTo(map);
 
     var baseMaps = {
         'OpenStreetMap': osm,
@@ -139,6 +152,33 @@ $.when(data_response).done(function() {
 
     L.control.layers(baseMaps, overlayMaps).addTo(map);
 
+    //legend
+    var legend = L.control({position: 'bottomright'});
+
+        legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = EqualInt,
+            labels = [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColorarray(grades[i] + 0.0001,EqualInt) + '"></i> ' + // so that d is not == EqualInt to respect function
+                (Math.floor(grades[i ]*100)/100) + ((Math.floor(grades[i + 1]*100)/100) ? ' &ndash; ' + (Math.floor(grades[i + 1]*100)/100) + '<br>' : ' et +');
+        }
+
+        return div;
+    };
+
+    console.log("Legend loaded", legend);
+
+    legend.addTo(map);
+
+    //scale
+    L.control.scale().addTo(map);
+
+    //interactions
     var info = L.control();
 
     info.onAdd = function (map) {
@@ -147,8 +187,7 @@ $.when(data_response).done(function() {
         return this._div;
     };
 
-    // method that we will use to update the control based on feature properties passed
-    info.update = function (parcels_f) {
+    info.update = function (parcels_f) {// method that we will use to update the control based on feature properties passed
         this._div.innerHTML = '<h4>Propriétés de la parcelle</h4>' +  (parcels_f ?
             '<b>' + (Math.round(parcels_f.i_multi_crit*1000)/1000) + '</b><br />' + parcels_f.area + ' area m<sup>2</sup>'
             : 'Survoler une parcelle');
